534f24e2d7fb1986589d1e86a7b7ad5c8a2384b2
Joe Simpson <headbangerkenny@googlemail.com>
2013-01-10 14:16:02 +0000

    We are in 2013 now

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Node ORM2'
-copyright = u'2012, Joe Simpson'
+copyright = u'2013, Joe Simpson'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
